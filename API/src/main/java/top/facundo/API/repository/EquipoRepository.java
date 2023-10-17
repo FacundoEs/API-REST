6da3139bb00c1d79675e56b52b0533c02e0fe719
FacundoEs <114000555+FacundoEs@users.noreply.github.com>
2023-10-17 08:27:37 -0300

    13

--- API/src/main/java/top/facundo/API/repository/EquipoRepository.java
+++
@@ -1,7 +0,0 @@
-package top.facundo.API.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import top.facundo.API.models.EquipoModel;
-
-public interface EquipoRepository extends JpaRepository<EquipoModel, Integer> {
-}
--- API/src/main/java/top/facundo/API/repository/InscripcionRepository.java
+++
@@ -1,7 +0,0 @@
-package top.facundo.API.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import top.facundo.API.models.InscripcionModel;
-
-public interface InscripcionRepository extends JpaRepository<InscripcionModel, Integer> {
-}
