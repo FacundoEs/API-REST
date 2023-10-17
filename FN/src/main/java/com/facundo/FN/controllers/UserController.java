fbdf5a5e0d1ff8c052e28f4f98bed0b99a8aff39
FacundoEs <114000555+FacundoEs@users.noreply.github.com>
2023-10-17 09:52:16 -0300

    Pruebas security

---
+++ FN/src/main/java/com/facundo/FN/controllers/UserController.java
@@ -0,0 +1,7 @@
+package com.facundo.FN.controllers;
+
+import com.facundo.FN.models.UserModel;
+import com.facundo.FN.services.UserService;
+
+public class UserController {
+}
---
+++ FN/src/main/java/com/facundo/FN/models/UserModel.java
@@ -0,0 +1,4 @@
+package com.facundo.FN.models;
+
+public class UserModel {
+}
